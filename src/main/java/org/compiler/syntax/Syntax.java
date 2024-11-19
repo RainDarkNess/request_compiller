45e1a5d56ec3ad68f4a51d3f83a4ca7a18235538
RainDarkNess <pooplolo52@gmail.com>
2024-11-19 16:38:10 +0400

    syntax done

--- src/main/java/org/compiler/syntax/Syntax.java
+++ src/main/java/org/compiler/syntax/Syntax.java
@@ -23,9 +23,15 @@ public class Syntax {
     int startTokens = 0;
     boolean isStarted = false;
     boolean varAction = false;
+    boolean varOperation = false;
+    boolean hasOperator = false;
     boolean programStart = false;
     boolean typeCheck = false;
     boolean assignVar = false;
+
+    // End.
+    boolean hasEnd = false;
+    boolean hasDot = false;
 
     // Condition booleans
     boolean chkVar = false;
@@ -33,9 +39,19 @@ public class Syntax {
 
     // If booleans
     boolean isIf = false;
-    int ifBody = 0;
+    int conditionBody = 0;
     boolean thenDetect = false;
 
+    // While booleans
+    boolean isWhile = false;
+    boolean doDetect = false;
+
+    // For booleans
+    boolean isFor = false;
+    boolean variableCreation = false;
+    String variableIdTmp = "";
+    boolean asDetect = false;
+    boolean toDetect = false;
     int tmp_type = 0;
     ArrayList<String> tmp_var_name = new ArrayList<>();
 
@@ -72,20 +88,52 @@ public class Syntax {
         };
     }
     public void varAssign(String buffer) throws CustomException{
+        int tableNum = Integer.parseInt(buffer.split(",")[0]);
+        if(buffer.equals("2,13")){
+            varAction = false;
+            return;
+        }
         for(String var: tmp_var_name) {
             for(Variable variable : variables){
                 if (Objects.equals(variable.name, String.valueOf(var))) {
                     if(variable.init){
                         return;
                     }
-                    variable.value = Integer.parseInt((String) tableForAll.get(0).get(Integer.parseInt(buffer.split(",")[1])));
+                    if(tableNum == 3){
+                        ArrayList arr = tableForAll.get(1);
+                        String var_tmp_name = tableForAll.get(1).get(Integer.parseInt(buffer.split(",")[1])).toString();
+                        for(Variable _variable_ : variables){
+                            if(Objects.equals(_variable_.name, var_tmp_name)){
+                                variable.value = _variable_.value;
+                            }
+                        }
+                    }else if(tableNum == 4){
+                        variable.value = Integer.parseInt((String) tableForAll.get(0).get(Integer.parseInt(buffer.split(",")[1])));
+                    }
                     variable.init = true;
                     debug.show("Переменной " + String.valueOf(var) + " присвоено значение " + variable.value);
+                    varOperation = true;
+                    hasOperator = false;
                     return;
                 }
             }
         }
-        throw new CustomException("Ошибка! Переменная не существует ");
+        if(tableNum == 4){
+            debug.show("Найдено значение в выражении " + writeMessageByBuffer(buffer));
+            varOperation = true;
+            hasOperator = false;
+            return;
+        }//                umn                     del                     &&                      add
+        if(buffer.equals("2,7") || buffer.equals("2,8") || buffer.equals("2,9") || buffer.equals("2,24")){
+            debug.show("Найден оператор действия над переменной " + writeMessageByBuffer(buffer));
+            hasOperator = true;
+            if(varOperation)
+                varOperation = false;
+            else
+                throw new CustomException("Неожиданный символ " + writeMessageByBuffer(buffer));
+            return;
+        }
+        throw new CustomException("Ошибка! Переменная не существует " + writeMessageByBuffer(buffer));
     }
 
     public boolean varCheck(String buffer){
@@ -133,29 +181,129 @@ public class Syntax {
 
     }
 
+    public void varCreate(String buffer){
+        Variable variable = new Variable();
+        variable.name = tableForAll.get(1).get(Integer.parseInt(buffer.split(",")[1])).toString();
+        variable.init = true;
+        variable.value = 0;
+        variable.type = 0;
+        variables.add(variable);
+    }
+
+    private void forBodyCreate(String buffer) throws CustomException {
+        int tableNum = Integer.parseInt(buffer.split(",")[0]);
+        int tableVal = Integer.parseInt(buffer.split(",")[1]);
+        if(toDetect){
+            if (tableNum == 3) {
+                debug.show("Найдена переменная в for для to: " + writeMessageByBuffer(buffer));
+                varCheck(buffer);
+                return;
+            }else if(tableNum == 4){
+                debug.show("Найдено значение в for для to: " + writeMessageByBuffer(buffer));
+                valueCheck(buffer);
+                return;
+            }
+            if(!doDetect){
+                if (buffer.equals("1,6")) {
+                    debug.show("Найдено do в for");
+                    doDetect = true;
+                    toDetect = false;
+                    asDetect = false;
+                    variableIdTmp = "";
+                    variableCreation = false;
+                    isFor = false;
+                    return;
+                }else{
+                    throw new CustomException("Ошибка! Неожиданный символ в for в конце объявления цикла!." + writeMessageByBuffer(buffer));
+                }
+            }
+            return;
+        }
+        if(!variableCreation) {
+            if (tableNum == 3) {
+                variableCreation = true;
+                variableIdTmp = buffer;
+                debug.show("Найдена переменная для for " + writeMessageByBuffer(buffer));
+
+            } else {
+                throw new CustomException("Ошибка! В for нет объявляемой переменной!." + writeMessageByBuffer(buffer));
+            }
+        }else{
+            if(!asDetect) {
+                if (buffer.equals("1,9")) {
+                    asDetect = true;
+                    debug.show("Найден as в for ");
+
+                }else{
+                    throw new CustomException("Ошибка! В for нет обязательного слова as!." + writeMessageByBuffer(buffer));
+                }
+            }else{
+                if (tableNum == 3) {
+                    debug.show("Найдена переменная в for " + writeMessageByBuffer(buffer));
+                    varCheck(buffer);
+                }else if(tableNum == 4){
+                    debug.show("Найдено значение в for " + writeMessageByBuffer(buffer));
+                    valueCheck(buffer);
+                }else if(!toDetect){
+                    if(buffer.equals("1,7")){
+                        debug.show("Найдено to в for ");
+                        toDetect = true;
+                        variableCreation = false;
+                    }else{
+                        throw new CustomException("Ошибка! Неожиданный символ в for!." + writeMessageByBuffer(buffer));
+                    }
+                }else{
+                    throw new CustomException("Ошибка! Неожиданный символ в for!." + writeMessageByBuffer(buffer));
+                }
+                varCreate(variableIdTmp);
+                tmp_var_name.add((String) tableForAll.get(1).get(Integer.parseInt(variableIdTmp.split(",")[1])));
+                varAssign(buffer);
+            }
+        }
+
+    }
+
     public void syntaxCodeBody(String buffer)throws CustomException{
         String[] words = Syntax.words.split(" ");
         String[] delimiters = Syntax.delimiters.split(" ");
 
+        if(Objects.equals(buffer, "1,3")){
+            hasEnd = true;
+            return;
+        }
+        if(Objects.equals(buffer, "2,12")){
+            if(hasEnd){
+                debug.show("Программа остановлена");
+                if(conditionBody>0){
+                    throw new CustomException("Ошибка! Незакрытая скобка составного оператора" + writeMessageByBuffer(buffer));
+                }
+            }else{
+                throw new CustomException("Ошибка! Случайностоящая точка" + writeMessageByBuffer(buffer));
+            }
+        }
+        if(hasEnd && !Objects.equals(buffer, "2,12")){
+            throw new CustomException("Ошибка! незавершенный " + writeMessageByBuffer(buffer));
+        }
         if(Integer.parseInt(buffer.split(",")[0]) == 3){
             varAction = true;
         }
 
         if (Objects.equals(buffer, "2,21")) {
-            if(!thenDetect){
+            if(!thenDetect && !doDetect){
                 throw new CustomException("Ошибка! Неожиданная открывающаяся скобка!." + writeMessageByBuffer(buffer));
             }
-            debug.show("Открыта скобка if");
-            ifBody++;
+            debug.show("Открыта скобка");
+            conditionBody++;
             thenDetect = false;
-        }else if(thenDetect && !Objects.equals(buffer, "2,21")){
+            doDetect = false;
+        }else if((thenDetect || doDetect) && !Objects.equals(buffer, "2,21")){
             throw new CustomException("Ошибка! Открывающаяся скобка в составном операторе не найдена!." + writeMessageByBuffer(buffer));
         }
 
         if (Objects.equals(buffer, "2,22")) {
-            debug.show("Закрыта скобка if");
-            if(ifBody > 0 ){
-                ifBody--;
+            debug.show("Закрыта скобка");
+            if(conditionBody > 0 ){
+                conditionBody--;
             }else{
                 throw new CustomException("Ошибка! Неожиданная закрывающаяся скобка!." + writeMessageByBuffer(buffer));
             }
@@ -169,6 +317,7 @@ public class Syntax {
             thenDetect = true;
             debug.show("Найден then. Тело if начинается");
             isIf = false;
+            chkVar = false;
         }
         if (Objects.equals(buffer, "1,8")) {
             thenDetect = true;
@@ -176,22 +325,65 @@ public class Syntax {
             isIf = false;
         }
 
+        if(isFor){
+            forBodyCreate(buffer);
+            return;
+        }
+
+        if (Objects.equals(buffer, "1,13")) {
+            debug.show("Найден for");
+            isFor = true;
+        }
+
+        if (Objects.equals(buffer, "1,6")) {
+            if(chkOperator){
+                throw new CustomException("Ошибка! Неверное условие в while." + writeMessageByBuffer(buffer));
+            }
+            doDetect = true;
+            debug.show("Найден do. Тело while начинается");
+            isWhile = false;
+            chkVar = false;
+        }
+
         if(isIf){
             condition(buffer);
             return;
         }
+        if(isWhile){
+            condition(buffer);
+            return;
+        }
+
 
         if (Objects.equals(buffer, "1,4")) {
             debug.show("Найден if");
             isIf = true;
         }
 
+        if(Objects.equals(buffer, "1,5")){
+            debug.show("Найден while");
+            isWhile = true;
+        }
+        int tableNum = Integer.parseInt(buffer.split(",")[0]);
         if(varAction){
             if (Objects.equals(buffer, "2,23")) {
                 debug.show("Найдено несколько переменных");
+                varOperation = false;
             } else if (Integer.parseInt(buffer.split(",")[0]) == 3) {
-                debug.show("Идет операция над переменной: " + writeMessageByBuffer(buffer));
-                tmp_var_name.add((String) tableForAll.get(1).get(Integer.parseInt(buffer.split(",")[1])));
+                if(!varOperation){
+                    debug.show("Идет операция над переменной: " + writeMessageByBuffer(buffer));
+                    tmp_var_name.add((String) tableForAll.get(1).get(Integer.parseInt(buffer.split(",")[1])));
+                    varOperation = true;
+                }else{
+                    throw new CustomException("Отсуствие обязательного слово 'as'" + writeMessageByBuffer(buffer));
+                }
+            } else if (Integer.parseInt(buffer.split(",")[0]) == 4) {
+                if(!varOperation){
+                    debug.show("Идет операция с значением: " + writeMessageByBuffer(buffer));
+                    varOperation = true;
+                }else{
+                    throw new CustomException("Отсуствие обязательного разделительного слова. Найдено: " + writeMessageByBuffer(buffer));
+                }
             }
 
             //                            int                                 float                                 bool
@@ -216,15 +408,14 @@ public class Syntax {
             }
 
             if(assignVar){
-                if(Integer.parseInt(buffer.split(",")[0]) == 4){
-                    varAssign(buffer);
-                }
+                varAssign(buffer);
                 tmp_var_name.clear();
             }
             //                              as
             if (Objects.equals(buffer, "1,9")) {
                 debug.show("Идет присвоение переменной: " + tmp_var_name.get(0));
                 assignVar = true;
+                varOperation = false;
             }
         }
         //                              ;
@@ -232,10 +423,14 @@ public class Syntax {
             if(typeCheck && programStart){
                 throw new CustomException("Ошибка! В данном модальном языке может быть объявлен только 1 тип данных.");
             }
+            if(hasOperator)
+                throw new CustomException("Ошибка! Ошибка в выражении.");
+
             varAction = false;
             programStart = true;
             typeCheck = false;
             assignVar = false;
+            varOperation = false;
         }
 
     }
